8b2b4470478b44a34db526fb98b8b0c5b0c214fc
Sebastian Christ <rudolfo.christ@gmail.com>
2024-07-08 15:37:27 +0200

    Ensure only HTTPS urls are used and show a flash

--- app/controllers.lisp
+++ app/controllers.lisp
@@ -9,6 +9,32 @@
 (defparameter +up.html+ (djula:compile-template* "up.html"))
 (defparameter +bookmarks-index.html+ (djula:compile-template* "bookmarks-index.html"))
 
+(defun render (view &key status arguments destination)
+  (when status
+    (setf (ht:return-code*) status))
+  (apply #'djula:render-template* view destination
+         :flash (ht:session-value :flash)
+         arguments))
+
+(defun flash (type message)
+  (let* ((session (ht:start-session))
+         (flash (ht:session-value :flash session)))
+    (setf (ht:session-value :flash session)
+          (cons (cons type message) flash))))
+
+;;; delete flash after it is used.
+(defmethod ht:handle-request :after (acceptor request)
+  (ht:delete-session-value :flash))
+
+(define-condition booker-error (error)
+  ((reason :initarg :reason
+           :reader booker-error-reason)))
+
+(define-condition http-url-found (booker-error)
+  ((url :initarg :url
+        :reader http-url-found-url))
+  (:default-initargs
+   :reason "URLs must use HTTPS"))
 
 (ht:define-easy-handler (up :uri (match :get "/up"))
     ()
@@ -17,7 +43,7 @@
      (jzon:stringify
       (alexandria:plist-hash-table (list :application :ok))))
     (t
-     (djula:render-template* +up.html+ nil))))
+     (render +up.html+))))
 
 (ht:define-easy-handler (home :uri "/")
     ()
@@ -32,10 +58,11 @@
 (ht:define-easy-handler (bookmarks-index :uri (match :get "/bookmarks"))
     ()
   (let ((bookmarks (apply-search-filter (params :q))))
-    (djula:render-template* +bookmarks-index.html+ nil
-                            :bookmarks bookmarks)))
+    (render +bookmarks-index.html+ :arguments (list :bookmarks bookmarks))))
 
 (defun retrieve-bookmark-content (url)
+  (when (uiop:string-prefix-p "http://" url)
+    (error  'http-url-found :url url))
   (let* ((html (dexador:get url))
          (dom (plump:parse html))
          (main (or (first (plump:get-elements-by-tag-name dom "main"))
@@ -51,15 +78,23 @@
 
 (ht:define-easy-handler (bookmarks-create :uri (match :post "/bookmarks"))
     ()
-  (multiple-value-bind (title body)
-      (retrieve-bookmark-content (params :url))
-    (booker/db:create-bookmark title (params :url) body))
-  (redirect "/bookmarks"))
+  (handler-case
+      (multiple-value-bind (title body)
+          (retrieve-bookmark-content (params :url))
+        (booker/db:create-bookmark title (params :url) body)
+        (flash "info" "Bookmark created!")
+        (redirect "/bookmarks"))
+    (http-url-found (condition)
+      (ht:log-message* :error "Error processing URL ~A" (http-url-found-url condition))
+      (flash "danger" (booker-error-reason condition))
+      (setf (ht:return-code*) ht:+http-bad-request+)
+      (bookmarks-index))))
 
 
 (ht:define-easy-handler (bookmarks-destory :uri (match :delete "/bookmarks/:id"))
     ()
   (when (booker/db:find-bookmark (params :id))
     (booker/db:delete-bookmark (params :id))
+    (flash "info" "Bookmark deleted!")
     (redirect "/bookmarks")))
 
