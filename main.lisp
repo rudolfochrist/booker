e6696cb49706106c71ac69c2de4a0a372a079bac
Sebastian Christ <rudolfo.christ@proton.me>
2025-10-24 19:54:16 +0200

    Add sighup to reload application and config

--- main.lisp
+++ main.lisp
@@ -2,6 +2,13 @@
 ;;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+(require 'asdf)
+
+(asdf:initialize-source-registry
+ `(:source-registry
+   (:tree . (uiop:getcwd))
+   :ignore-inherited-configuration))
+
 (defpackage #:booker/main
   (:use :cl)
   (:export
@@ -14,10 +21,16 @@
   (booker:stop-application)
   (uiop:quit))
 
+(defun reload (signo info-sap context-sap)
+  (declare (ignore signo info-sap context-sap))
+  (asdf:load-system "booker" :force t)
+  (booker:reload-config))
+
 (defun main (&optional args)
   (declare (ignore args))
   (sb-sys:enable-interrupt sb-unix:sigint #'handle-interrupt)
   (sb-sys:enable-interrupt sb-unix:sigterm #'handle-interrupt)
+  (sb-sys:enable-interrupt sb-unix:sighup #'reload)
   (booker:start-application t))
 
 (push #'main sb-ext:*init-hooks*)
