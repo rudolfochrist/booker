2e9b035bb6079d14f9288ae9b1ddde049ff315cd
Sebastian Christ <rudolfo.christ@proton.me>
2025-10-25 17:48:31 +0200

    Keep screen session running if sbcl process dies

--- run-prod.sh
+++ run-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 app_env="${APP_ENV:-production}"
-APP_ENV="$app_env" screen -UdmS booker sbcl --core booker.image --no-userinit
+APP_ENV="$app_env" screen -UdmS booker /bin/sh -c 'sbcl --core booker.image --no-userinit; exec sh'
